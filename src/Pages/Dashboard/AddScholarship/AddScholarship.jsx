bf94a28253b845adb276de0ed1bc53715f13baba
Dilara Hossenara <dilarahossenara@gmail.com>
2025-02-23 00:07:34 +0600

    add scholership

--- src/Pages/Dashboard/AddScholarship/AddScholarship.jsx
+++ src/Pages/Dashboard/AddScholarship/AddScholarship.jsx
@@ -93,6 +93,7 @@ const AddScholarship = () => {
         }
     };
 
+    
     return (
         <div className="p-6 mx-auto max-w-3xl bg-white shadow-lg rounded-lg">
             <h2 className="text-3xl font-bold mb-6 text-center text-green-600">Add Scholarship</h2>
