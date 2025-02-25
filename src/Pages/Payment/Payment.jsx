a390ab51376fddd329c43b89e4c6e5f7c3507202
Dilara Hossenara <dilarahossenara@gmail.com>
2025-02-25 16:44:15 +0600

    update

--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -3,7 +3,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import CheckoutForm from "./CheckoutForm";
 // todo : add key
-const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PK_KEY);
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 
 const Payment = () => {
     const loaderData = useLoaderData()
